9c590b8732cadd771dadc0543389a2e8da4ac3ad
Arfer0030 <aufarafli1234@gmail.com>
2025-05-09 10:44:08 +0700

    tes commit

--- app/src/main/java/com/example/calmme/MainActivity.kt
+++ app/src/main/java/com/example/calmme/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             CalmMeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "CalmMe",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
