ecc2dca6615fc931eb10cbf832ab628781c8bd87
Arfer0030 <aufarafli1234@gmail.com>
2025-06-11 15:15:46 +0700

    splash screen

--- app/src/main/java/com/example/calmme/SplashActivity.kt
+++ app/src/main/java/com/example/calmme/SplashActivity.kt
@@ -3,22 +3,20 @@ package com.example.calmme
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Handler
 import android.os.Looper
+import androidx.activity.ComponentActivity
 
 @SuppressLint("CustomSplashScreen")
-class SplashScreenActivity : AppCompatActivity() {
-    private val splashTimeOut: Long = 2000 // 2 detik
-
+class SplashActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(R.layout.activity_splash)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, MainActivity::class.java))
             finish()
-        }, splashTimeOut)
+        }, 1000)
     }
 }
+
